b233b4f4134860e1ad8f3d7d81686df892f77873
gahla <gahlawat80@gmail.com>
2022-12-20 10:30:05 -0500

    JWT security changes

--- src/main/java/com/learn2code/vehicle/api/search/config/AppConfig.java
+++ src/main/java/com/learn2code/vehicle/api/search/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.learn2code.vehicle.api.search.config;
 
+import com.learn2code.vehicle.api.search.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +19,8 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -28,7 +33,9 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/api/v1/manufacturers").hasAnyRole("USER","ADMIN")
                 .antMatchers("/api/v1/vechicle-market-price").hasRole("ADMIN")
-                .anyRequest().permitAll().and().httpBasic();
+                .anyRequest().permitAll().and().httpBasic()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
---
+++ src/main/java/com/learn2code/vehicle/api/search/dto/LoginCredentails.java
@@ -0,0 +1,13 @@
+package com.learn2code.vehicle.api.search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginCredentails {
+    private String username;
+    private String password;
+}
